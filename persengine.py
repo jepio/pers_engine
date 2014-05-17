babfd78cf5792b85598130f0ef7c81d76e11cb3a
jepio <jeremi.piotrowski@gmail.com>
2014-05-17 22:53:51 +0200

    Fixed main_old()

--- persengine.py
+++ persengine.py
@@ -7,10 +7,10 @@ def main_old():
     graph_data2 = [(2, 3, 0, 1), (0, 6, 2, 8)]
     graph_data = [graph_data1, graph_data2]
     name = "tree.xml"
-    root = persxml.createindex(keynames)
+    root = iohandler.xh.createindex(keynames)
     for i in xrange(2):
-        persxml.creategraph(root, graph_data[i], keynames[i], 2)
-    persxml.writexml(root, name)
+        iohandler.xh.creategraph(root, graph_data[i], keynames[i], 2)
+    iohandler.xh.writexml(root, name)
 
 
 def main():
