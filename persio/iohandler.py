01b0f1f2f20fa99336a5b1ea53983ab5840f9ffb
jepio <jeremi.piotrowski@gmail.com>
2014-05-20 13:01:21 +0200

    Considered more general filename cases.

--- persio/iohandler.py
+++ persio/iohandler.py
@@ -12,6 +12,8 @@ class IOHandler:
         """
         self.input = filename
         self.output = filename.split('.')[0] + '.xml'
+        self.converted = False
+        self.loaded = False
 
     def root2xml(self):
         """
@@ -21,20 +23,25 @@ class IOHandler:
         rootfile, keynames = rh.openroot(self.input)
         xmlfile = xh.createindex(keynames)
         keynames_copy = keynames[:]
+
         for graphname in keynames_copy:
             graph = rootfile.Get(graphname)
             if type(graph) is not rh.TGraphErrors:
-                print "Removed object: ", graph
+#                print "Removed object: ", graph
                 keynames.remove(graphname)
                 continue
             array, number = rh.graphtonp(graph)
             xh.creategraph(xmlfile, array, graphname, number)
+
         index = xmlfile.find("index")
         keynames.sort()
         index.text = ' '.join(keynames)
         xh.writexml(xmlfile, self.output)
+        self.converted = True
 
     def xml2mem(self, xmlfilename=None):
+        if self.converted:
+            xmlfilename = xmlfilename or self.output
         self.tree = xh.ET.parse(xmlfilename)
         self.root = self.tree.getroot()
         self.index = self.root[0].text.split()
