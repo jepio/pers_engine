d434bb4b968efdde2ad7d2765dd373f22d3840f5
jepio <jeremi.piotrowski@gmail.com>
2014-05-20 18:45:18 +0200

    Moved unused testing function to a place that is more aesthetically pleasing. Plan on removing it later on.

--- persengine.py
+++ persengine.py
@@ -1,17 +1,6 @@
+#!/usr/bin/env python2
 from persio import iohandler
 import persinterface as ui
-
-
-def main_old():
-    keynames = ["A", "B"]
-    graph_data1 = [(0, 0, 0, 1), (0, 1, 2, 3)]
-    graph_data2 = [(2, 3, 0, 1), (0, 6, 2, 8)]
-    graph_data = [graph_data1, graph_data2]
-    name = "tree.xml"
-    root = iohandler.xh.createindex(keynames)
-    for i in xrange(2):
-        iohandler.xh.creategraph(root, graph_data[i], keynames[i], 2)
-    iohandler.xh.writexml(root, name)
 
 
 def main():
@@ -23,3 +12,17 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+"""
+def main_old():
+    keynames = ["A", "B"]
+    graph_data1 = [(0, 0, 0, 1), (0, 1, 2, 3)]
+    graph_data2 = [(2, 3, 0, 1), (0, 6, 2, 8)]
+    graph_data = [graph_data1, graph_data2]
+    name = "tree.xml"
+    root = iohandler.xh.createindex(keynames)
+    for i in xrange(2):
+        iohandler.xh.creategraph(root, graph_data[i], keynames[i], 2)
+    iohandler.xh.writexml(root, name)
+"""
