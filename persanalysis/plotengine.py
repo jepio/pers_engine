629423da38a8c077a02cc51248815df4b70d6c11
jepio <jeremi.piotrowski@gmail.com>
2014-05-24 21:22:01 +0200

    Started working on plotengine.

---
+++ persanalysis/plotengine.py
@@ -0,0 +1,10 @@
+""" Module for plotting graphs to file """
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import pylab
+
+
+class PlotEngine(object):
+
+    """ Class that performs saving of plots """
+    pass
