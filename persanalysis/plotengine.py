51df5913d9473e1b64e927e66b85aab403f1e58f
jepio <jeremi.piotrowski@gmail.com>
2014-05-24 23:06:49 +0200

    Wrote plotting engine

--- persanalysis/plotengine.py
+++ persanalysis/plotengine.py
@@ -1,10 +1,27 @@
 """ Module for plotting graphs to file """
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import pylab
 
 
 class PlotEngine(object):
 
     """ Class that performs saving of plots """
-    pass
+
+    def __init__(self, data_tuple, name):
+        xval, yval, xerrors, yerrors = data_tuple
+        self.output_name = name
+        plt.errorbar(xval, yval, xerr=xerrors, yerr=yerrors)
+        plt.title(name)
+        self.labels()
+
+    def save(self, extension):
+        """ Save the plot with extension """
+        name = self.output_name + "." + extension
+        plt.savefig(name, bbox_inches="tight")
+        plt.close()
+
+    @staticmethod
+    def labels(xlabel=r'temperature ($^\circ$C)',
+               ylabel="current (mA)"):
+        """ Label the axes """
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
