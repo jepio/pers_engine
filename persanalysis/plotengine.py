7b715fb64cc935bb06368099f1be21391b763d83
jepio <jeremi.piotrowski@gmail.com>
2014-05-24 23:44:27 +0200

    Plotting style changes: no lines, point markers and higher dpi for png images.

--- persanalysis/plotengine.py
+++ persanalysis/plotengine.py
@@ -1,5 +1,6 @@
 """ Module for plotting graphs to file """
 import matplotlib.pyplot as plt
+# import numpy as np
 
 
 class PlotEngine(object):
@@ -8,20 +9,24 @@ class PlotEngine(object):
 
     def __init__(self, data_tuple, name):
         xval, yval, xerrors, yerrors = data_tuple
+        self.xval = xval
         self.output_name = name
-        plt.errorbar(xval, yval, xerr=xerrors, yerr=yerrors)
-        plt.title(name)
-        self.labels()
+        plt.errorbar(xval, yval, xerr=xerrors, yerr=yerrors, marker=".",
+                     linestyle="None")
+        plt.title("IT graph for sensor " + name)
+        self.style()
 
     def save(self, extension):
         """ Save the plot with extension """
         name = self.output_name + "." + extension
-        plt.savefig(name, bbox_inches="tight")
+        plt.savefig(name, bbox_inches="tight", dpi=96 * 2)
         plt.close()
+        return name
 
     @staticmethod
-    def labels(xlabel=r'temperature ($^\circ$C)',
-               ylabel="current (mA)"):
+    def style(xlabel=r'temperature ($^\circ$C)',
+              ylabel="current (mA)"):
         """ Label the axes """
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
+        # plt.xlim(np.min(self.xval), np.max(self.xval))
